d800776e1a99e296f4ce572a6637b4703dbebd25
WanLittle <2621996061@qq.com>
2019-01-19 21:33:28 +0800

    feat: output .png by "svpng.inc"

---
+++ RayTracingAWeekend/main.cpp
@@ -0,0 +1,122 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "svpng.inc"
+
+#include "vec3.h"
+#include "ray.h"
+#include "sphere.h"
+#include "hitable.h"
+#include "hitable_list.h"
+#include "camera.h"
+#include "ds_random.h"
+#include "material.h"
+#include "lambertian.h"
+#include "metal.h"
+#include "dielectric.h"
+
+#define MAXFLOAT 100
+
+vec3 color(const ray& r, hitable *world, int depth);
+hitable *random_scene();
+
+int main()
+{
+	int nx = 160;
+	int ny = 80;
+	int ns = 1;
+	unsigned char *img = new unsigned char[nx * ny * 3];
+
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10.0; //焦距
+	float aperture = 0.1; //光圈半径
+
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
+
+	hitable *world = random_scene();
+
+	int k = 0;
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++)
+			{
+				float u = float(i + drand48()) / float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world, 0);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); // gamma校正
+			img[k++] = unsigned char(255.99 * col[0]); //r
+			img[k++] = unsigned char(255.99 * col[1]); //g
+			img[k++] = unsigned char(255.99 * col[2]); //b
+		}
+	}
+
+	svpng(fopen("D:/Desktop/Ray Tracing.png", "wb"), nx, ny, img, 0); //将结果输出到png图片中
+
+	return 0;
+}
+
+vec3 color(const ray& r, hitable *world, int depth)
+{
+	hit_record rec;
+	if (world->hit(r, 0.001, MAXFLOAT, rec))
+	{
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+		{
+			return attenuation * color(scattered, world, depth + 1);
+		}
+		else
+		{
+			return vec3(0, 0, 0);
+		}
+	}
+	else
+	{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0); //混合白色和蓝色
+	}
+}
+
+hitable *random_scene()
+{
+	int n = 500;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = drand48();
+			vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {  // 漫反射
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+				}
+				else if (choose_mat < 0.95) { // 金属
+					list[i++] = new sphere(center, 0.2,
+						new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+				}
+				else {  // 玻璃
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
